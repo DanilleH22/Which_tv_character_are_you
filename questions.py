b0eb8e76bdb56a3d8a33adb76cd9825d0baf8255
DanilleH22 <>
2023-09-12 16:11:55 +0000

    feat(questions.py): Added new file to store questions

---
+++ questions.py
@@ -0,0 +1,184 @@
+# Questions to ask user to determine personality
+
+questions = [
+    {
+        "question": "What's your sense of humor like?",
+        "answers": [
+            "a. Sarcastic",
+            "b. Slapstick",
+            "c. Witty",
+            "d. Dark and twisted"
+        ]
+    },
+    {
+        "question": "What's your preferred mode of transportation?",
+        "answers": [
+            "a. Car",
+            "b. Walking",
+            "c. Bicycle",
+            "d. Public Transport"
+        ]
+    },
+    {
+        "question": "Choose a recreational activity:",
+        "answers": [
+            "a. Playing video games",
+            "b. Fishing",
+            "c. Skiing/snowboarding",
+            "d. Karaoke"
+        ]
+    },
+    {
+        "question": "What's your favorite food?",
+        "answers": [
+            "a. Pizza",
+            "b. Tacos",
+            "c. Burgers",
+            "d. Sushi"
+        ]
+    },
+    {
+        "question": "What's your favorite school subject?",
+        "answers": [
+            "a. None, I hate school",
+            "b. Science or math",
+            "c. Social studies or current events",
+            "d. Art or creative writing "
+        ]
+    },
+    {
+        "question": "How do you handle conflict?",
+        "answers": [
+            "a. Confront it head-on",
+            "b. Avoid it at all costs",
+            "c. Seek mediation and compromise",
+            "d. Use humor to defuse tension "
+        ]
+    },
+    {
+        "question": "Pick a holiday destination:",
+        "answers": [
+            "a. Beach resort",
+            "b. Cultural city",
+            "c. Ski resort",
+            "d. Remote wilderness"
+        ]
+    },
+    {
+        "question": "What's your stance on authority figures?",
+        "answers": [
+            "a. Challenge and question ",
+            "b. Respect and Obey",
+            "c. Make fun of them",
+            "d. Ignore them"
+        ]
+    },
+    {
+        "question": "How do you feel about school/work?",
+        "answers": [
+            "a. Try to follow rules and guidelines",
+            "b. Enjoy it",
+            "c. Tolerate it",
+            "d. Dread it"
+        ]
+    },
+    {
+        "question": "What type of pet would you prefer?",
+        "answers": [
+            "a. Snake",
+            "b. Dog",
+            "c. Cat",
+            "d. Hamster"
+        ]
+    },
+    {
+        "question": "What's your favorite TV show genre?",
+        "answers": [
+            "a. Science fiction",
+            "b. Comedy",
+            "c. Drama",
+            "d. Horror"
+        ]
+    },
+    {
+        "question": "What's your preferred way to express your feelings?",
+        "answers": [
+            "a. Manipulate and scheme",
+            "b. Be the voice of reason",
+            "c. Get caught up in ridiculous adventures",
+            "d. Try to keep the peace"
+        ]
+    },
+    {
+        "question": "Choose a favorite activity during recess:",
+        "answers": [
+            "a. Through outbursts and tantrums",
+            "b. Through art",
+            "c. Through speeches and activism",
+            "d. Through music"
+        ]
+    },
+    {
+        "question": "Pick a favorite subject in school:",
+        "answers": [
+            "a. Lunchtim",
+            "b. English adn Writing",
+            "c. math",
+            "d. Recess"
+        ]
+    },
+    {
+        "question": "Choose a superpower:",
+        "answers": [
+            "a. Super strength",
+            "b. Telepaths",
+            "c. Invisibility",
+            "d. Immortal "
+        ]
+    },
+    {
+        "question": "How do you react to authority figures in your life?",
+        "answers": [
+            "a. Rebel and question their authority",
+            "b. Follow their rules and advice",
+            "c. Seek their approval",
+            "d. Ignore them and do your own thing"
+        ]
+    },
+    {
+        "question": "What's your favorite way to spend free time?",
+        "answers": [
+            "a. Playing video games",
+            "b. Hanging out with friends",
+            "c. Reading books",
+            "d. Going on adventures"
+        ]
+    },
+    {
+        "question": "Pick a favorite South Park episode:",
+        "answers": [
+            "a. Scott Tenorman Must Die",
+            "b. Make Love, Not Warcraft",
+            "c. Imaginationland",
+            "d. The Coon"
+        ]
+    },
+    {
+        "question": "What's your approach to problem-solving?",
+        "answers": [
+            "a. Manipulate and deceive others",
+            "b. Make inpulsive decisions",
+            "c. Try to follow rules and guidelines",
+            "d. Think logically and rationally"
+        ]
+    },
+    {
+        "question": "Choose a role in a group project:",
+        "answers": [
+            "a. The bossy leader",
+            "b. The dependable team player",
+            "c. The wildcard who suggests crazy ideas",
+            "d. The eager helper who follows instructions"
+        ]
+    }
+]
